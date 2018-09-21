c97ae8d1a26c92c2196df0594bf698cd2eb22bc3
LeeJaeYeop <birdman9391@postech.ac.kr>
2018-09-21 19:42:27 +0900

    Class_defined

---
+++ GameClass.cpp
@@ -0,0 +1,237 @@
+#include "GameClass.h"
+using namespace std;
+
+float distance(float x1, float y1, float x2, float y2)
+{
+	float dx, dy;
+	dx = x1 - x2;
+	dy = y1 - y2;
+	return sqrt(pow(dx, 2) + pow(dy, 2));
+}
+
+Map::Map() {
+	//initializing Map! 한섭 is doing this!
+}
+void Map::setmap(int x, int y, int value)
+{
+	map[x][y] = value;
+}
+int Map::getmap(int n, int m)
+{
+	return map[n][m];
+}
+
+
+
+User::User() {
+	direction = 2;
+}
+User::User(float x, float y) {
+	pos_x = x;
+	pos_y = y;
+	direction = 2;
+}
+void User::move()
+{
+	//
+}
+//private 다루는 함수는 다 알아서 만들기
+void User::set_direction(int dir)
+{
+	direction = dir;
+}
+int User::get_direction()
+{
+	return direction;
+}
+float User::get_pos_x()
+{
+	return pos_x;
+}
+float User::get_pos_y()
+{
+	return pos_y;
+}
+void User::set_pos(float x, float y)
+{
+	pos_x = x;
+	pos_y = y;
+}
+int User::get_item_num()
+{
+	return item_num;
+}
+//아이템 개수를 return
+void User::set_item_num(int n)
+{
+	item_num = n;
+}
+//아이템 개수를 n개로 맞춰주는 함수
+void User::increase_item_num()
+{
+	item_num++;
+}
+//유저가 아이템을 먹었을때 아이템개수를 증가시키는 함수
+void User::decrease_item_num()
+{
+	item_num--;
+}
+//유저가 아이템을 사용했을 때 아이템개수를 감소시키는 함수
+bool User::isvalidDirection(int dir)
+{
+	switch (dir)
+	{
+	case 0:
+		//북
+		if (Map::getmap(int(pos_x - 0.5), int(pos_y + 0.5)) == 1 || Map::getmap(int(pos_x + 0.5), int(pos_y + 0.5)) == 1)
+			return false;
+		return true;
+	case 1:
+		//동
+		if (Map::getmap(int(pos_x + 0.5), int(pos_y + 0.5)) == 1 || Map::getmap(int(pos_x + 0.5), int(pos_y - 0.5)) == 1)
+			return false;
+		return true;
+	case 2:
+		//남
+		if (Map::getmap(int(pos_x - 0.5), int(pos_y - 0.5)) == 1 || Map::getmap(int(pos_x + 0.5), int(pos_y - 0.5)) == 1)
+			return false;
+		return true;
+	case 3:
+		//서
+		if (Map::getmap(int(pos_x - 0.5), int(pos_y + 0.5)) == 1 || Map::getmap(int(pos_x - 0.5), int(pos_y - 0.5)) == 1)
+			return false;
+		return true;
+	}
+}
+//dir 방향으로 현재 움직일수 있는지 확인해서 움직일수 있으면 true를 return한다
+
+
+
+
+
+
+Enemy::Enemy() {
+	is_aggro = 0;
+	enemy_list.push_back(this);
+}
+Enemy::Enemy(float x, float y)
+{
+	pos_x = x;
+	pos_y = y;
+	enemy_list.push_back(this);
+}
+void Enemy::move() {
+	//
+}//is_aggro 세팅된거에 따라 무브를 한다. 플레이어와 가까워지면 플레이어를 죽인다! 총알에 의해 죽는건 총알클래스에 넣어놓을거니 구현 미필요. 
+ //벽으로는 못가게해야함. 아이템은 못먹음. 시간마다 랜덤하게 움직여야함. 기준은 구현자가 알아서 하자!
+float Enemy::get_pos_x()
+{
+	return pos_x;
+}
+float Enemy::get_pos_y()
+{
+	return pos_y;
+}
+void Enemy::set_pos(float x, float y)
+{
+	pos_x = x;
+	pos_y = y;
+}
+Enemy* Enemy::get_enemy(int i)
+{
+	return enemy_list[i];
+}
+//i번째 enemy를 불러옴
+vector<Enemy*> Enemy::get_enemy_list()
+{
+	return enemy_list;
+}
+//enemy_list를 불러옴
+void Enemy::remove_enemy(int i)
+{
+	enemy_list.erase(enemy_list.begin() + i);
+}
+//i 번째 enemy를 삭제하는 함수
+int Enemy::get_enemy_num()
+{
+	return enemy_list.size();
+}
+bool Enemy::isUserNear()
+{
+	return distance(pos_x, pos_y, User::get_pos_x(), User::get_pos_y()) <= AGGRO_DISTANCE;
+}
+//유저가 가까운 경우 true를 return한다. 거리 기준은 AGGRO_DISTANCE보다 가까운 경우를 기준으로 함
+bool Enemy::isUserCollide() {
+	return distance(pos_x, pos_y, User::get_pos_x(), User::get_pos_y()) <= DEATH_DISTANCE;
+}
+//유저와 충돌한 경우 true를 return한다. 거리 기준은 DEATH_DISTANCE보다 가까운 경우를 기준으로 함.
+bool Enemy::isvalidDirection(int dir)
+{
+	switch (dir)
+	{
+	case 0:
+		//북
+		if (Map::getmap(int(pos_x - 0.5), int(pos_y + 0.5)) == 1 || Map::getmap(int(pos_x + 0.5), int(pos_y + 0.5)) == 1)
+			return false;
+		return true;
+	case 1:
+		//동
+		if (Map::getmap(int(pos_x + 0.5), int(pos_y + 0.5)) == 1 || Map::getmap(int(pos_x + 0.5), int(pos_y - 0.5)) == 1)
+			return false;
+		return true;
+	case 2:
+		//남
+		if (Map::getmap(int(pos_x - 0.5), int(pos_y - 0.5)) == 1 || Map::getmap(int(pos_x + 0.5), int(pos_y - 0.5)) == 1)
+			return false;
+		return true;
+	case 3:
+		//서
+		if (Map::getmap(int(pos_x - 0.5), int(pos_y + 0.5)) == 1 || Map::getmap(int(pos_x - 0.5), int(pos_y - 0.5)) == 1)
+			return false;
+		return true;
+	}
+}
+//vector<Enemy*> Enemy::enemy_list = vector<Enemy*>();
+
+
+
+Bullet::Bullet(float x, float y, int dir) {
+	pos_x = x;
+	pos_y = y;
+	direction = dir;
+	velocity = BULLET_VELOCITY;
+}
+void Bullet::move()//enemy랑 부딪히면 enemy죽이고 총알 사라짐, 벽이랑 부딪혀도 사라짐
+{
+	///
+}
+int Bullet::check_state()
+{
+	if (Map::getmap(int(pos_x), int(pos_y)) == 1)
+	{
+		return -2;
+	}
+	else
+	{
+		for (int i = 0; i < Enemy::get_enemy_num(); i++)
+		{
+			if (distance(pos_x, pos_y, Enemy::get_enemy(i)->get_pos_x(), Enemy::get_enemy(i)->get_pos_y()) <= DEATH_DISTANCE)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+}//일반적인 상황에선 -1을 return, 벽에 부딪힐경우 -2를 return, enemy랑 부딪힐때는 enemy의 index를 return
+
+
+
+
+
+
+int User::direction;
+float User::pos_x;
+float User::pos_y;
+int User::item_num;
+int Map::map[100][100];
+vector<Enemy*> Enemy::enemy_list;
---
+++ GameClass.h
@@ -0,0 +1,97 @@
+#pragma once
+
+#include <windows.h>
+#include <gl/gl.h>/
+#include <gl/glut.h>
+#include <vector>
+#include<iostream>
+#include<math.h>
+
+#define DEATH_DISTANCE 0.5
+#define AGGRO_DISTANCE 5
+#define BULLET_VELOCITY 0.005
+using namespace std;
+
+float distance(float x1, float y1, float x2, float y2);
+class Map
+{
+private:
+	static int map[100][100];//0 : 빈곳, 1: 벽, 2: User, 3: Enemy, 4: Item
+							 //여기서 게임을 진행하자!
+public:
+	Map();
+	static void setmap(int x, int y, int value);
+	static int getmap(int n, int m);
+};
+
+class User
+{
+private:
+	static int direction;// 0:북, 1: 동, 2: 남, 3: 서 
+						 //User의 direction은 default로 남쪽(2)으로 설정되어 있다.
+	static float pos_x, pos_y;
+	static int item_num;//유저가 먹은 아이템의 개수
+public:
+	User();
+	User(float x, float y);
+	void move();
+	//private 다루는 함수는 다 알아서 만들기
+	static void set_direction(int dir);
+	static int get_direction();
+	static float get_pos_x();
+	static float get_pos_y();
+	static void set_pos(float x, float y);
+	static int get_item_num();
+	//아이템 개수를 return
+	static void set_item_num(int n);
+	//아이템 개수를 n개로 맞춰주는 함수
+	static void increase_item_num();
+	//유저가 아이템을 먹었을때 아이템개수를 증가시키는 함수
+	static void decrease_item_num();
+	//유저가 아이템을 사용했을 때 아이템개수를 감소시키는 함수
+	bool isvalidDirection(int dir);
+	//dir 방향으로 현재 움직일수 있는지 확인해서 움직일수 있으면 true를 return한다
+};
+class Enemy
+{
+private:
+	float pos_x, pos_y;
+	bool is_aggro;//0 : 어그로 안끌림, 1: 어그로 끌림.  default로 어그로 안끌린 상태(0)로 설정되어 있음
+	static vector<Enemy*> enemy_list;
+public:
+	Enemy();
+	Enemy(float x, float y);
+	void move();
+	 //벽으로는 못가게해야함. 아이템은 못먹음. 시간마다 랜덤하게 움직여야함. 기준은 구현자가 알아서 하자!
+	float get_pos_x();
+	float get_pos_y();
+	void set_pos(float x, float y);
+	static Enemy* get_enemy(int i);
+	//i번째 enemy를 불러옴
+	static vector<Enemy*> get_enemy_list();
+	//enemy_list를 불러옴
+	static void remove_enemy(int i);
+	//i 번째 enemy를 삭제하는 함수
+	static int get_enemy_num();
+	bool isUserNear();
+	//유저가 가까운 경우 true를 return한다. 거리 기준은 AGGRO_DISTANCE보다 가까운 경우를 기준으로 함
+	bool isUserCollide();
+	//유저와 충돌한 경우 true를 return한다. 거리 기준은 DEATH_DISTANCE보다 가까운 경우를 기준으로 함.
+	bool isvalidDirection(int dir);
+	//dir 방향으로 현재 움직일수 있는지 확인해서 움직일수 있으면 true를 return한다
+};
+//vector<Enemy*> Enemy::enemy_list = vector<Enemy*>();
+
+
+class Bullet
+{
+private:
+	float pos_x, pos_y;
+	int direction;
+	int velocity;
+public:
+	Bullet() {}
+	Bullet(float x, float y, int dir);
+	void move();
+	int check_state();
+};
